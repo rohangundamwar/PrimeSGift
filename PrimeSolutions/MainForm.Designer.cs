6d25289338e44a0d8f89e5c234712f5d7401d1db
rohangundamwar <rohangundamwar@yahoo.co.in>
2017-04-30 17:54:24 +0530

    Files Uploaded

---
+++ PrimeSolutions/MainForm.Designer.cs
@@ -0,0 +1,295 @@
+﻿namespace PrimeSolutions
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.saleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.purchaseToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.accountingToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openingBalanceToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.creditToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.debitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.balanceSheetToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.reportToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saleToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.customerDailyReportToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.purchaseToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.supplierDailyReportToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.supplierPaymentToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.duplicateBillToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saleToolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.purchaseToolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.settingToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lbl_developer = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.Gainsboro;
+            this.menuStrip1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.saleToolStripMenuItem,
+            this.purchaseToolStripMenuItem,
+            this.accountingToolStripMenuItem,
+            this.reportToolStripMenuItem,
+            this.settingToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(110, 351);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // saleToolStripMenuItem
+            // 
+            this.saleToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
+            this.saleToolStripMenuItem.Name = "saleToolStripMenuItem";
+            this.saleToolStripMenuItem.Padding = new System.Windows.Forms.Padding(8);
+            this.saleToolStripMenuItem.Size = new System.Drawing.Size(97, 39);
+            this.saleToolStripMenuItem.Text = "Sale";
+            // 
+            // purchaseToolStripMenuItem
+            // 
+            this.purchaseToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
+            this.purchaseToolStripMenuItem.Name = "purchaseToolStripMenuItem";
+            this.purchaseToolStripMenuItem.Padding = new System.Windows.Forms.Padding(8);
+            this.purchaseToolStripMenuItem.Size = new System.Drawing.Size(97, 39);
+            this.purchaseToolStripMenuItem.Text = "Purchase";
+            // 
+            // accountingToolStripMenuItem
+            // 
+            this.accountingToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openingBalanceToolStripMenuItem,
+            this.creditToolStripMenuItem,
+            this.debitToolStripMenuItem,
+            this.balanceSheetToolStripMenuItem});
+            this.accountingToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
+            this.accountingToolStripMenuItem.Name = "accountingToolStripMenuItem";
+            this.accountingToolStripMenuItem.Padding = new System.Windows.Forms.Padding(8);
+            this.accountingToolStripMenuItem.Size = new System.Drawing.Size(97, 39);
+            this.accountingToolStripMenuItem.Text = "Accounting";
+            // 
+            // openingBalanceToolStripMenuItem
+            // 
+            this.openingBalanceToolStripMenuItem.Name = "openingBalanceToolStripMenuItem";
+            this.openingBalanceToolStripMenuItem.Size = new System.Drawing.Size(191, 24);
+            this.openingBalanceToolStripMenuItem.Text = "Opening Balance";
+            // 
+            // creditToolStripMenuItem
+            // 
+            this.creditToolStripMenuItem.Name = "creditToolStripMenuItem";
+            this.creditToolStripMenuItem.Size = new System.Drawing.Size(191, 24);
+            this.creditToolStripMenuItem.Text = "Credit";
+            // 
+            // debitToolStripMenuItem
+            // 
+            this.debitToolStripMenuItem.Name = "debitToolStripMenuItem";
+            this.debitToolStripMenuItem.Size = new System.Drawing.Size(191, 24);
+            this.debitToolStripMenuItem.Text = "Debit";
+            // 
+            // balanceSheetToolStripMenuItem
+            // 
+            this.balanceSheetToolStripMenuItem.Name = "balanceSheetToolStripMenuItem";
+            this.balanceSheetToolStripMenuItem.Size = new System.Drawing.Size(191, 24);
+            this.balanceSheetToolStripMenuItem.Text = "Balance Sheet";
+            // 
+            // reportToolStripMenuItem
+            // 
+            this.reportToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.saleToolStripMenuItem1,
+            this.purchaseToolStripMenuItem1,
+            this.duplicateBillToolStripMenuItem});
+            this.reportToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
+            this.reportToolStripMenuItem.Name = "reportToolStripMenuItem";
+            this.reportToolStripMenuItem.Padding = new System.Windows.Forms.Padding(8);
+            this.reportToolStripMenuItem.Size = new System.Drawing.Size(97, 39);
+            this.reportToolStripMenuItem.Text = "Report";
+            // 
+            // saleToolStripMenuItem1
+            // 
+            this.saleToolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.customerDailyReportToolStripMenuItem});
+            this.saleToolStripMenuItem1.Name = "saleToolStripMenuItem1";
+            this.saleToolStripMenuItem1.Size = new System.Drawing.Size(166, 24);
+            this.saleToolStripMenuItem1.Text = "Sale";
+            // 
+            // customerDailyReportToolStripMenuItem
+            // 
+            this.customerDailyReportToolStripMenuItem.Name = "customerDailyReportToolStripMenuItem";
+            this.customerDailyReportToolStripMenuItem.Size = new System.Drawing.Size(230, 24);
+            this.customerDailyReportToolStripMenuItem.Text = "Customer Daily Report";
+            // 
+            // purchaseToolStripMenuItem1
+            // 
+            this.purchaseToolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.supplierDailyReportToolStripMenuItem,
+            this.supplierPaymentToolStripMenuItem});
+            this.purchaseToolStripMenuItem1.Name = "purchaseToolStripMenuItem1";
+            this.purchaseToolStripMenuItem1.Size = new System.Drawing.Size(166, 24);
+            this.purchaseToolStripMenuItem1.Text = "Purchase";
+            // 
+            // supplierDailyReportToolStripMenuItem
+            // 
+            this.supplierDailyReportToolStripMenuItem.Name = "supplierDailyReportToolStripMenuItem";
+            this.supplierDailyReportToolStripMenuItem.Size = new System.Drawing.Size(247, 24);
+            this.supplierDailyReportToolStripMenuItem.Text = "Supplier Daily Report";
+            this.supplierDailyReportToolStripMenuItem.Click += new System.EventHandler(this.supplierDailyReportToolStripMenuItem_Click);
+            // 
+            // supplierPaymentToolStripMenuItem
+            // 
+            this.supplierPaymentToolStripMenuItem.Name = "supplierPaymentToolStripMenuItem";
+            this.supplierPaymentToolStripMenuItem.Size = new System.Drawing.Size(247, 24);
+            this.supplierPaymentToolStripMenuItem.Text = "Supplier Payment Report";
+            // 
+            // duplicateBillToolStripMenuItem
+            // 
+            this.duplicateBillToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.saleToolStripMenuItem2,
+            this.purchaseToolStripMenuItem2});
+            this.duplicateBillToolStripMenuItem.Name = "duplicateBillToolStripMenuItem";
+            this.duplicateBillToolStripMenuItem.Size = new System.Drawing.Size(166, 24);
+            this.duplicateBillToolStripMenuItem.Text = "Duplicate Bill";
+            // 
+            // saleToolStripMenuItem2
+            // 
+            this.saleToolStripMenuItem2.Name = "saleToolStripMenuItem2";
+            this.saleToolStripMenuItem2.Size = new System.Drawing.Size(138, 24);
+            this.saleToolStripMenuItem2.Text = "Sale ";
+            // 
+            // purchaseToolStripMenuItem2
+            // 
+            this.purchaseToolStripMenuItem2.Name = "purchaseToolStripMenuItem2";
+            this.purchaseToolStripMenuItem2.Size = new System.Drawing.Size(138, 24);
+            this.purchaseToolStripMenuItem2.Text = "Purchase";
+            // 
+            // settingToolStripMenuItem
+            // 
+            this.settingToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.settingToolStripMenuItem.Margin = new System.Windows.Forms.Padding(8, 12, 8, 12);
+            this.settingToolStripMenuItem.Name = "settingToolStripMenuItem";
+            this.settingToolStripMenuItem.Size = new System.Drawing.Size(81, 21);
+            this.settingToolStripMenuItem.Text = "Setting";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Padding = new System.Windows.Forms.Padding(8);
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(97, 39);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.ErrorImage = null;
+            this.pictureBox2.Image = global::PrimeSolutions.Properties.Resources.Prime_logo;
+            this.pictureBox2.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox2.InitialImage")));
+            this.pictureBox2.Location = new System.Drawing.Point(478, 292);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(183, 50);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(113, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(556, 285);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lbl_developer
+            // 
+            this.lbl_developer.AutoSize = true;
+            this.lbl_developer.Location = new System.Drawing.Point(386, 292);
+            this.lbl_developer.Name = "lbl_developer";
+            this.lbl_developer.Size = new System.Drawing.Size(83, 26);
+            this.lbl_developer.TabIndex = 3;
+            this.lbl_developer.Text = "Developed and \r\nMaintained by";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(669, 351);
+            this.Controls.Add(this.lbl_developer);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Main";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem saleToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem purchaseToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem accountingToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem reportToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saleToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem customerDailyReportToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem purchaseToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem supplierDailyReportToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem supplierPaymentToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem duplicateBillToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saleToolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem purchaseToolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem openingBalanceToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem creditToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem debitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem balanceSheetToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem settingToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label lbl_developer;
+    }
+}
+
