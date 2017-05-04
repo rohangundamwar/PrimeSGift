22273fadc4f3f2be68f4c6d0fcac0f923e3ff415
rohangundamwar <rohangundamwar@yahoo.co.in>
2017-05-04 23:28:15 +0530

    Purchase Form Upload

---
+++ PrimeSolutions/Purchase/frm_PurchaceForm.cs
@@ -0,0 +1,333 @@
+﻿using PrimeSolutions.Library;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+namespace PrimeSolutions
+{
+    public partial class frm_PurchaseForm : Form
+    {
+        public frm_PurchaseForm()
+        {
+            InitializeComponent();
+        }
+
+        Simplevalidations _objSimpal = new Simplevalidations();
+        SQLHelper _objSQLHelper = new SQLHelper();
+
+        private void frm_PurchaseForm_Load(object sender, EventArgs e)
+        {
+            Masterclear();
+            cmb_Name.Select();
+        //    txt_BillNo.Text = _objSQLHelper.gmGetMstID("P", "0");
+        }
+
+        private void Masterclear()
+        {
+            cmb_Name.ResetText();
+            cmb_Category.ResetText();
+            cmb_SubCategory.ResetText();
+            txt_AccNo.ResetText();
+            txt_Address.ResetText();
+            txt_BalAmt.ResetText();
+            txt_BillNo.ResetText();
+            txt_City.ResetText();
+            txt_ContactNo.ResetText();
+            txt_MobileNo.ResetText();
+            txt_NetAmt.ResetText();
+            txt_PaidAmt.ResetText();
+            txt_Qty.Text = "0";
+            txt_TotalAmt.ResetText();
+            txt_Vat.ResetText();
+            txt_SellingAmt.Text = "0";
+            txt_PurchaseAmt.Text = "0";
+            
+        }
+
+        private void cmb_Name_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode ==  Keys.Enter)
+            {
+                txt_Address.Focus();
+            }
+
+        }
+
+        private void txt_Address_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                txt_City.Focus();
+            }
+        }
+
+        private void txt_City_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                txt_ContactNo.Focus();
+            }
+        }
+
+        private void txt_ContactNo_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                txt_MobileNo.Focus();
+            }
+        }
+
+        private void txt_MobileNo_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                txt_BillNo.Focus();
+            }
+        }
+
+        private void txt_BillNo_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                cmb_Category.Focus();
+            }
+
+        }
+
+        private void cmb_Category_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                cmb_SubCategory.Focus();
+            }
+        }
+
+        private void cmb_SubCategory_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                txt_PurchaseAmt.Focus();
+            }
+        }
+
+       
+
+        private void txt_Qty_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                txt_Amt.Focus();
+            }
+        }
+        
+        private void bttn_Add_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                txt_TotalAmt.Focus();
+            }
+        }
+
+        private void txt_TotalAmt_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                txt_Vat.Focus();
+            }
+        }
+
+        private void txt_Vat_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                txt_NetAmt.Focus();
+            }
+        }
+
+        private void txt_NetAmt_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                txt_PaidAmt.Focus();
+            }
+        }
+
+        private void txt_PaidAmt_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                txt_BalAmt.Focus();
+            }
+        }
+
+        private void txt_BalAmt_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                bttn_Purchase.Focus();
+            }
+        }
+
+        private void bttn_Close_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            this.Close();
+        }
+
+        private void txt_PurchaseAmt_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                txt_SellingAmt.Focus();
+            }
+        }
+
+        private void txt_SellingAmt_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                txt_Qty.Focus();
+            }
+        }
+
+        private void txt_Amt_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                bttn_Add.Focus();
+            }
+        }
+
+        private void cmb_Name_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            _objSimpal.ValidationCharOnly(e);
+        }
+
+        private void txt_Address_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            _objSimpal.ValidationCharDigitOnly(e);
+        }
+
+        private void txt_City_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            _objSimpal.ValidationCharOnly(e);
+        }
+
+        private void txt_ContactNo_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            _objSimpal.ValidationDigitOnly(e);
+        }
+
+        private void txt_MobileNo_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            _objSimpal.ValidationDigitOnly(e);
+        }
+
+        private void txt_BillNo_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            _objSimpal.ValidationCharDigitOnly(e);
+        }
+
+        private void cmb_Category_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            _objSimpal.ValidationCharOnly(e);
+        }
+
+        private void cmb_SubCategory_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            _objSimpal.ValidationCharOnly(e);
+        }
+
+        private void txt_PurchaseAmt_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            _objSimpal.ValidationDigitWithPoint(e, txt_PurchaseAmt.Text);
+        }
+
+        private void txt_SellingAmt_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            _objSimpal.ValidationDigitWithPoint(e, txt_SellingAmt.Text);
+        }
+
+        private void txt_Qty_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            _objSimpal.ValidationDigitOnly(e);
+        }
+
+        private void txt_Amt_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            _objSimpal.ValidationDigitWithPoint(e, txt_Amt.Text);
+        }
+
+        private void txt_TotalAmt_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            _objSimpal.ValidationDigitWithPoint(e, txt_TotalAmt.Text);
+        }
+
+        private void txt_Vat_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            _objSimpal.ValidationDigitWithPoint(e, txt_Vat.Text);
+        }
+
+        private void txt_NetAmt_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            _objSimpal.ValidationDigitWithPoint(e, txt_NetAmt.Text);
+        }
+
+        private void txt_PaidAmt_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            _objSimpal.ValidationDigitWithPoint(e, txt_PaidAmt.Text);
+        }
+
+        private void txt_BalAmt_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            _objSimpal.ValidationDigitWithPoint(e, txt_BalAmt.Text);
+        }
+
+        private void txt_Qty_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                txt_Amt.Text = (Convert.ToDouble(txt_PurchaseAmt.Text) * Convert.ToDouble(txt_Qty.Text)).ToString();
+            }
+            catch { }
+        }
+
+        private void txt_PurchaseAmt_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                txt_Amt.Text = (Convert.ToDouble(txt_PurchaseAmt.Text) * Convert.ToDouble(txt_Qty.Text)).ToString();
+            }
+            catch (Exception ex)
+            { }
+        }
+
+        private void bttn_Add_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                
+                dgv_ItemInfo.Rows.Add(cmb_Category.Text, cmb_SubCategory.Text, txt_PurchaseAmt.Text, txt_Qty.Text, txt_SellingAmt.Text, txt_Amt.Text);
+                Clear();
+                
+            }
+            catch(Exception ex)
+            {
+
+            }
+        }
+
+        private void Clear()
+        {
+            cmb_Category.ResetText();
+            cmb_SubCategory.ResetText();
+            txt_Qty.Text = "0";
+            txt_SellingAmt.Text = "0";
+            txt_PurchaseAmt.Text = "0";
+        }
+    }
+}
